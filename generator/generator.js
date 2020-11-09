2c8be15d78f5ba4f40aae4bf050a5648176d6c54
Suzclem Adriana Ochoa Casillas <15171173@itculiacan.edu.mx>
2020-11-08 17:05:06 -0700

    separating carrers

--- generator/generator.js
+++ generator/generator.js
@@ -2,10 +2,6 @@ var csv = require("csv");
 const fs = require("fs");
 const PptxGenJS = require("pptxgenjs");
 
-function getTimestamp() {
-  return Date.now();
-}
-
 const main = () => {
   const fileData = fs.readFileSync("../data/titulaciones.csv", {
     encoding: "utf-8",
@@ -15,105 +11,118 @@ const main = () => {
     const pptx = new PptxGenJS();
     pptx.layout = "LAYOUT_WIDE";
 
-    data.forEach((item) => {
-      const slide = pptx.addSlide();
+    var carreras = data
+      .map(({ carrera }) => {
+        return carrera;
+      })
+      .reduce((unique, item) => {
+        return unique.includes(item) ? unique : [...unique, item];
+      }, []);
 
-      slide
-        .addImage({
-          path: `../assets/headers/logo.png`,
-          x: "2.5%",
-          y: 0,
-          w: 2878,
-          h: 321,
-          sizing: { type: "contain", w: "55%", h: "20%" },
-        })
-        .addText(
-          [
-            {
-              text: "Carrera",
-              options: {
-                fontSize: 30,
-                color: "1c3467",
-                bold: true,
-              },
-            },
-          ],
-          {
-            x: 0,
-            y: "25%",
-            w: "60%",
-            h: "10%",
-            align: "center",
-          }
-        )
-        .addText(
-          [
-            {
-              text: item["carrera"],
-              options: {
-                fontSize: 25,
-                color: "1c3467",
-              },
-            },
-          ],
-          {
-            x: 0,
-            y: "38%",
-            w: "60%",
-            h: "20%",
-            align: "center",
-          }
-        )
-        .addText(
-          [
-            {
-              text: "Generación 2015-2019",
-              options: {
-                fontSize: 30,
-                color: "1c3467",
-                bold: true,
-              },
-            },
-          ],
-          {
-            x: 0,
-            y: "60%",
-            w: "60%",
-            h: "10%",
-            align: "center",
-          }
-        )
-        .addImage({
-          path: `../assets/img/${Object.values(item)[0]}.jpeg`,
-          x: "60%",
-          y: 0,
-          w: "40%",
-          h: "90%",
-          sizing: { type: "contain" },
-        })
-        .addText(
-          [
-            {
-              text: item["Nombre"],
-              options: {
-                fontSize: 27,
-                color: "ffffff",
-                bold: true,
-              },
-            },
-          ],
-          {
-            x: 0,
-            y: "90%",
-            w: "100%",
-            h: "10%",
-            align: "center",
-            fill: { color: "1c3467" },
-          }
-        );
+    carreras.forEach((carrera) => {
+      data
+        .filter(({ carreraAlumno }) => carreraAlumno === carrera)
+        .forEach((item) => {
+          console.log(carrera);
+          const slide = pptx.addSlide();
+
+          slide
+            .addImage({
+              path: `../assets/headers/logo.png`,
+              x: "2.5%",
+              y: 0,
+              w: 2878,
+              h: 321,
+              sizing: { type: "contain", w: "55%", h: "20%" },
+            })
+            .addText(
+              [
+                {
+                  text: "Carrera",
+                  options: {
+                    fontSize: 30,
+                    color: "1c3467",
+                    bold: true,
+                  },
+                },
+              ],
+              {
+                x: 0,
+                y: "25%",
+                w: "60%",
+                h: "10%",
+                align: "center",
+              }
+            )
+            .addText(
+              [
+                {
+                  text: item["carrera"],
+                  options: {
+                    fontSize: 25,
+                    color: "1c3467",
+                  },
+                },
+              ],
+              {
+                x: 0,
+                y: "38%",
+                w: "60%",
+                h: "20%",
+                align: "center",
+              }
+            )
+            .addText(
+              [
+                {
+                  text: "Generación 2015-2019",
+                  options: {
+                    fontSize: 30,
+                    color: "1c3467",
+                    bold: true,
+                  },
+                },
+              ],
+              {
+                x: 0,
+                y: "60%",
+                w: "60%",
+                h: "10%",
+                align: "center",
+              }
+            )
+            .addImage({
+              path: `../assets/img/${carrera}/${Object.values(item)[0]}.jpeg`,
+              x: "60%",
+              y: 0,
+              w: "40%",
+              h: "90%",
+              sizing: { type: "contain" },
+            })
+            .addText(
+              [
+                {
+                  text: item["Nombre"],
+                  options: {
+                    fontSize: 27,
+                    color: "ffffff",
+                    bold: true,
+                  },
+                },
+              ],
+              {
+                x: 0,
+                y: "90%",
+                w: "100%",
+                h: "10%",
+                align: "center",
+                fill: { color: "1c3467" },
+              }
+            );
+        });
     });
 
-    pptx.writeFile("PptxGenJS-Sandbox-").then(function (fileName) {
+    pptx.writeFile(carrera).then(function (fileName) {
       console.log("Saved! File Name: " + fileName);
     });
   });
